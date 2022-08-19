fbfe2fb0bd31e03f432304dd68e80fa55d52e9c4
Casey Hudler <chudler624@gmail.com>
2022-08-19 14:19:52 -0400

    initial push

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Teams;
+
+var team1 = new NFLTeam();
+
+    team1.name = "Patriots";
+    team1.location = "New England";
+    team1.ranking = 1;
+    team1.haveWonSuperBowl = true;
+    team1.SuperBowlsWon = 6;
+
+var team2 = new NFLTeam();
+
+    team2.name = "Steelers";
+    team2.location = "Pittsburg";
+    team2.ranking = 2;
+    team2.haveWonSuperBowl = true;
+    team2.SuperBowlsWon = 6;
+
+var team3 = new NFLTeam();
+
+    team3.name = "Rams";
+    team3.location = "Los Angeles";
+    team3.ranking = 3;
+    team3.haveWonSuperBowl = true;
+    team3.SuperBowlsWon= 3;
+
+var team4 = new NFLTeam();
+    team4.name = "Cardinals";
+    team4.location = "Arizona";
+    team4.ranking = 24;
+    team4.haveWonSuperBowl = false;
+    team4.SuperBowlsWon = 0;
+
+
+
+List<string> NFLteamList = new List<string>();
+{ team1, team2, team3, team4}
+
+    
+    
+
+foreach (var team in NFLteamList)
+    Console.WriteLine();
---
+++ Teams.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teams
+{
+    public class NFLTeam
+    {
+        public string name { get; set; }    
+        public string location { get; set; }
+        public int ranking { get; set; }
+        public bool haveWonSuperBowl { get; set; }
+        public int SuperBowlsWon { get; set; }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
